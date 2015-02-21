ec6fa85a92f5f3d37bf040c12e620bb9c12d2645
Tom Stuart <tom@codon.com>
2015-02-21 14:31:20 +0000

    Implement Parser#has_more_commands?

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,17 @@
-module Parser
+class Parser
+  def initialize(input)
+    self.more_commands =
+      input.lines.
+        map { |line| line.sub(%r{//.*}, '') }.
+        map(&:strip).
+        reject(&:empty?)
+  end
+
+  def has_more_commands?
+    more_commands.any?
+  end
+
+  private
+
+  attr_accessor :more_commands
 end
