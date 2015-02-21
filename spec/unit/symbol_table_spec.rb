5f2b9dd382a2875bbce1f47c87341f21c3f62acc
Tom Stuart <tom@codon.com>
2015-02-21 18:59:25 +0000

    Add symbol table module examples

--- spec/unit/symbol_table_spec.rb
+++ spec/unit/symbol_table_spec.rb
@@ -1,4 +1,30 @@
 require 'symbol_table'
 
 RSpec.describe SymbolTable do
+  subject(:symbol_table) { SymbolTable.new }
+
+  context 'when empty' do
+    it 'doesn’t contain any symbols' do
+      expect(symbol_table.contains?('SCREEN')).to be_falsy
+      expect(symbol_table.contains?('KBD')).to be_falsy
+    end
+  end
+
+  context 'when a symbol has been added' do
+    before(:example) do
+      symbol_table.add_entry('SCREEN', 0x4000)
+    end
+
+    it 'contains that symbol' do
+      expect(symbol_table.contains?('SCREEN')).to be_truthy
+    end
+
+    it 'associates the correct address with that symbol' do
+      expect(symbol_table.get_address('SCREEN')).to eq 0x4000
+    end
+
+    it 'doesn’t contain a different symbol' do
+      expect(symbol_table.contains?('KBD')).to be_falsy
+    end
+  end
 end
