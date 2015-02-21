cbb3472652378e76e283b0534476bf8bea87c2b0
Tom Stuart <tom@codon.com>
2015-02-21 14:31:20 +0000

    Implement Parser#has_more_commands?

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,23 @@
-module Parser
+require 'strscan'
+
+class Parser
+  def initialize(input)
+    self.scanner = StringScanner.new(input)
+  end
+
+  def has_more_commands?
+    scanner.skip Patterns::WHITESPACE_AND_COMMENTS
+    !scanner.eos?
+  end
+
+  private
+
+  attr_accessor :scanner
+
+  module Patterns
+    WHITESPACE_AND_COMMENTS =
+      %r{
+        (?: [[:space:]] | // .* $ )+
+      }x
+  end
 end
