d3ddd50df52960916c822d9fa539aecdcb87405d
Tom Stuart <tom@codon.com>
2015-02-21 18:59:42 +0000

    Implement SymbolTable

--- lib/symbol_table.rb
+++ lib/symbol_table.rb
@@ -1,2 +1,17 @@
-module SymbolTable
+require 'forwardable'
+
+class SymbolTable
+  def initialize
+    self.contents = {}
+  end
+
+  extend Forwardable
+
+  def_delegator :contents, :[]=, :add_entry
+  def_delegator :contents, :has_key?, :contains?
+  def_delegator :contents, :[], :get_address
+
+  private
+
+  attr_accessor :contents
 end
