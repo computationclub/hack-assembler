d6fe9f02ad1845c4af3bad981001f0fc23f23fe4
Leo Cassarani <leo.cassarani@me.com>
2015-02-24 19:14:15 +0000

    Implement Parser#has_more_commands?

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,11 @@
-module Parser
+class Parser
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def has_more_commands?
+    !input.gsub(%r{//.*$}, '').strip.empty?
+  end
 end
