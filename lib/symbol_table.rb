3643b1b7a1da7fa34e453024f0f632e6e3c74b33
Leo Cassarani <leo.cassarani@me.com>
2015-02-24 20:17:45 +0000

    Implement SymbolTable

--- lib/symbol_table.rb
+++ lib/symbol_table.rb
@@ -1,2 +1,21 @@
-module SymbolTable
+class SymbolTable
+  def initialize
+    @symbols = Hash.new
+  end
+
+  def contains?(symbol)
+    symbols.key?(symbol)
+  end
+
+  def add_entry(symbol, address)
+    symbols[symbol] = address
+  end
+
+  def get_address(symbol)
+    symbols.fetch(symbol)
+  end
+
+  private
+
+  attr_reader :symbols
 end
