ad5fa7989664d9938b9bac4a2db6a0808219ca7e
Chris Lowis <chris.lowis@gmail.com>
2015-02-24 18:12:26 +0000

    Implement SymbolTable
    A hash seemed an appropriate structure for this.

--- lib/symbol_table.rb
+++ lib/symbol_table.rb
@@ -1,2 +1,17 @@
-module SymbolTable
+class SymbolTable
+  def initialize
+    @table = {}
+  end
+
+  def contains?(sym)
+    @table.has_key? sym
+  end
+
+  def add_entry(sym, address)
+    @table[sym] = address
+  end
+
+  def get_address(sym)
+    @table.fetch(sym)
+  end
 end
