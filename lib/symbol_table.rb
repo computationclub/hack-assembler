20f41d40c5bc191f4edaeb0b8c5184dbcd701473
Kevin Butler <haqkrs@gmail.com>
2015-03-05 03:42:40 +0000

    Impl SymbolTable

--- lib/symbol_table.rb
+++ lib/symbol_table.rb
@@ -1,2 +1,15 @@
-module SymbolTable
+class SymbolTable
+  def initialize
+    @table = {}
+  end
+
+  def add_entry symbol, value
+    @table[symbol] = value
+  end
+
+  def get_address symbol
+    @table[symbol]
+  end
+
+  alias contains? get_address
 end
