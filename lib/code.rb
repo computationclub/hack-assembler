4cbde0bbb56639641da65f3a2e2ed49c7ef68c98
Kevin Butler <haqkrs@gmail.com>
2015-03-05 03:02:53 +0000

    Impl Code#dest

--- lib/code.rb
+++ lib/code.rb
@@ -1,2 +1,8 @@
 module Code
+    def self.dest mnemonic
+        d1 = mnemonic.index('A') ? '1' : '0'
+        d2 = mnemonic.index('D') ? '1' : '0'
+        d3 = mnemonic.index('M') ? '1' : '0'
+        d1 + d2 + d3
+    end
 end
