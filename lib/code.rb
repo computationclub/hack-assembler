01eb9e590973e7f98aefba5aa90d7ae1cef65cc8
Kevin Butler <haqkrs@gmail.com>
2015-03-05 03:07:01 +0000

    Impl Code#jump

--- lib/code.rb
+++ lib/code.rb
@@ -5,4 +5,13 @@ module Code
         d3 = mnemonic.index('M') ? '1' : '0'
         d1 + d2 + d3
     end
+
+    def self.jump mnemonic
+        return "111" if mnemonic == "JMP"
+        return "101" if mnemonic == "JNE"
+        j1 = mnemonic.index('L') ? '1' : '0'
+        j2 = mnemonic.index('E') ? '1' : '0'
+        j3 = mnemonic.index('G') ? '1' : '0'
+        j1 + j2 + j3
+    end
 end
