9ed6185efaf88470d225bd208fc73c8220fb513b
Kevin Butler <haqkrs@gmail.com>
2015-03-05 00:31:06 +0000

    Impl Parser#advance

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,4 +6,10 @@ class Parser
     def has_more_commands?
         @remaining =~ /^\s*[^\s\/]+.*$/
     end
+
+    def advance
+        begin
+            @current, _, @remaining = @remaining.partition("\n")
+        end while @current =~ /^((\s*\/+.*)|\s*$)/
+    end
 end
