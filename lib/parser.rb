d39cd8731983db52ae8f38acf479be938ffcdff3
Kevin Butler <haqkrs@gmail.com>
2015-03-05 01:04:21 +0000

    Impl Parser#command_type

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,4 +1,8 @@
 class Parser
+    A_COMMAND = 4
+    L_COMMAND = 1
+    C_COMMAND = 8
+
     def initialize input
         @remaining = input
     end
@@ -11,5 +15,13 @@ class Parser
         begin
             @current, _, @remaining = @remaining.partition("\n")
         end while @current =~ /^((\s*\/+.*)|\s*$)/
+    end
+
+    def command_type
+        case @current.lstrip[0]
+        when '@' then Parser::A_COMMAND
+        when '(' then Parser::L_COMMAND
+        else Parser::C_COMMAND
+        end
     end
 end
